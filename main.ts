a0dcf0bb68a17aee5755f8bfc24fe4225aa4ae3c
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-04-14 16:35:53 -0400

    changed typescript code

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@
 
 // variables
 let sprite: game.LedSprite = null
-let loopCounterX: number = 0
-let loopCounterY: number = 0
+let sides: number = 0
+let steps: number = 0
 
 // show happy face
 basic.clearScreen()
@@ -16,32 +16,61 @@ basic.showIcon(IconNames.Happy)
 
 // when button "A" is pressed
 input.onButtonPressed(Button.A, function () {
+    // create the sprite at the beginning and wait for 0.1 seconds
     basic.clearScreen()
     sprite = game.createSprite(0, 0)
-    loopCounterX = 0
-    loopCounterY = 0
+    sides = 0
+    basic.pause(100)
 
-    // move the sprite right by 1 each time
-    while (loopCounterX < 5) {
-        sprite.set(LedSpriteProperty.X, loopCounterX)
-        loopCounterX++
-        basic.pause(500)
-        while (loopCounterX == 5) {
-            sprite.set(LedSpriteProperty.Y, loopCounterY)
-            loopCounterY++
-            basic.pause(500)
+    // if its not back to the position it started then continue code
+    while (sides < 4) {
+        // reset the steps to 0 so the sprite can move to the 4th coordinate before stopping
+        steps = 0
+        // if its not in the last pixel then move 1 step foward and wait for 0.1 seconds
+        while (steps < 4) {
+            sprite.move(1)
+            basic.pause(100)
+            steps++
         }
+        // turn right when it reaches the end
+        sprite.turn(Direction.Right, 90)
+        sides++
     }
-    loopCounterX--
 
-    while (loopCounterX >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounterX)
-        loopCounterX--
-        basic.pause(500)
-        while (loopCounterX == 0) {
-            sprite.set(LedSpriteProperty.Y, loopCounterY)
-            loopCounterY--
-            basic.pause(500)
+    // delete sprite and show happy face
+    sprite.delete()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// when button "B" is pressed
+input.onButtonPressed(Button.B, function () {
+    // create the sprite at the beginning and wait for 0.1 seconds
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    basic.pause(100)
+    sides = 0
+
+    // make the sprite face down before starting
+    sprite.turn(Direction.Right, 90)
+
+    // if its not back to the position it started then continue code
+    while (sides < 4) {
+        // reset the steps to 0 so the sprite can move to the 4th coordinate before stopping
+        steps = 0
+        // if its not in the last pixel then move 1 step foward and wait for 0.1 seconds
+        while (steps < 4) {
+            sprite.move(1)
+            basic.pause(100)
+            steps++
         }
+        // turn left when it reaches the end
+        sprite.turn(Direction.Left, 90)
+        sides++
     }
+
+    // delete sprite and show happy face
+    sprite.delete()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
 })
